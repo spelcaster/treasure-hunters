17e7f25fefcf622a509b97a3a010f346dd19cfeb
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-02-26 01:00:26 -0300

    [add] Game constants
    - This source file contains some constants that will be used across the
      game elements;

---
+++ include/Constants.hpp
@@ -0,0 +1,16 @@
+// Copyright 2015 <https://github.com/spelcaster>
+
+#ifndef __DCW_CONSTANTS_H__
+#define __DCW_CONSTANTS_H__
+
+/*!
+ * Game elements movement speed
+ */
+const float kMovementSpeed = 333.f;
+
+/*!
+ * The tile size 32x32
+ */
+const float kBlockSize = 32.f;
+
+#endif  // __DCW_CONSTANTS_H__
