e7eceb22728ca15b49d5473cf51305e748e98e7d
Hugo Augusto Freitas do Carmo <hugrito.kun@gmail.com>
2016-02-26 00:40:46 -0300

    [add] Class Terrain
    - Added headers and source files for Terrain class;
    - This class represents a world tile;

---
+++ Terrain.cpp
@@ -0,0 +1,29 @@
+// Copyright 2015 <https://github.com/spelcaster>
+
+#include "include/Terrain.hpp"
+
+//! Terrain constructor
+Terrain::Terrain() {
+}
+
+//! Terrain destructor
+Terrain::~Terrain() {
+}
+
+/*!
+ * @brief Sets the value of the movement cost
+ *
+ * @param[in] cost The movement cost of the tile
+ */
+void Terrain::setMovementCost(float cost) {
+    movement_cost = cost;
+}
+
+/*!
+ * @brief Gets the value of the movement cost
+ *
+ * @returns The movement cost of the tile
+ */
+const float Terrain::getMovementCost() {
+    return movement_cost;
+}
---
+++ include/Terrain.hpp
@@ -0,0 +1,41 @@
+// Copyright 2015 <https://github.com/spelcaster>
+
+#ifndef __DCW_TERRAIN_H__
+#define __DCW_TERRAIN_H__
+
+#include <SFML/Graphics.hpp>
+
+//! Terrain
+/*!
+ * The class Terrain represents one tile
+ */
+class Terrain: public sf::Sprite {
+ private:
+    /*!
+     * The movement cost of the tile
+     */
+    float movement_cost;
+
+ public:
+    //! Terrain constructor
+    Terrain();
+
+    //! Terrain destructor
+    ~Terrain();
+
+    /*!
+     * @brief Sets the value of the movement cost
+     *
+     * @param[in] cost The movement cost of the tile
+     */
+    void setMovementCost(float cost);
+
+    /*!
+     * @brief Gets the value of the movement cost
+     *
+     * @returns The movement cost of the tile
+     */
+    const float getMovementCost();
+};
+
+#endif  // __DCW_TERRAIN_H__
